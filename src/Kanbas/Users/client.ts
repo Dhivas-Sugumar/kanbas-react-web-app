42c2989f574190996f13af0faae09d6942da5198
Dhivas-Sugumar <dhivassugumar@gmail.com>
2024-04-15 16:47:38 -0400

    Fixed user errprs

--- src/Kanbas/Users/client.ts
+++ src/Kanbas/Users/client.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
-export const BASE_API = process.env.REACT_APP_BASE_API_URL;
-export const USERS_API = `${BASE_API}/api/users`;
+const API_BASE = process.env.REACT_APP_API_BASE;
+export const USERS_API = `${API_BASE}/api/users`;
 export interface User { _id: string; username: string; password: string; role: string;
 firstName: string, lastName: string };
 export const signin = async (credentials: User) => {
+  console.log(credentials);
+  console.log(`${USERS_API}/signin`);
   const response = await axios.post( `${USERS_API}/signin`, credentials );
   return response.data;
 };
