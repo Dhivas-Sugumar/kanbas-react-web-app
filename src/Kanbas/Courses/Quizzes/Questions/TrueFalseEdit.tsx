2762cc3b41756baf2f3e99d0d4bffbaf575b5246
samblock <block.sa@northeastern.edu>
2024-04-19 16:34:38 -0400

    made trueFalse edit component

--- src/Kanbas/Courses/Quizzes/Questions/TrueFalseEdit.tsx
+++ src/Kanbas/Courses/Quizzes/Questions/TrueFalseEdit.tsx
@@ -1,1 +1,37 @@
-export {}
+import { useDispatch, useSelector } from "react-redux";
+import { KanbasState } from "../../../store";
+import { useParams } from "react-router";
+import { setQuestion } from "./reducer";
+import { useEffect } from "react";
+
+const TrueFalseEdit = () => {
+
+    const question = useSelector((state: KanbasState) => state.questionsReducer.question);
+    const {quizId} = useParams();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(setQuestion({...question, type: "True/False", answer: "", choices: ["", ""]}));
+    }, [quizId])
+
+    const handleTrue = (answer: string) => {
+        dispatch(setQuestion({...question, answer: answer, choices:[answer, question.choices[1]]}));
+    }
+    const handleFalse = (answer: string) => {
+        dispatch(setQuestion({...question, choices:[question.choices[0], answer]}));
+    }
+    return (
+        <div>
+            <input type="text"
+            value={question.choices[0]}
+            onChange={(e)=> handleTrue(e.target.value)}/>
+            <label>True</label>
+            <input type="text"
+            value={question.choices[1]}
+            onChange={(e)=> handleFalse(e.target.value)}/>
+            <label>True</label>
+        </div>
+    )
+}
+
+export default TrueFalseEdit;
