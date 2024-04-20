062bcccd2c1eda91abc657084711125e65613477
samblock <block.sa@northeastern.edu>
2024-04-19 21:07:26 -0400

    finished edit question screens

--- src/Kanbas/Courses/Quizzes/Questions/MultipleBlanksEdit.tsx
+++ src/Kanbas/Courses/Quizzes/Questions/MultipleBlanksEdit.tsx
@@ -1,9 +1,34 @@
+import { useDispatch } from "react-redux";
 import { Question } from "../../../store";
+import { setQuestion } from "./reducer";
 
 const MultipleBlanksEdit = ({question}: {question: Question}) => {
+
+  const dispatch = useDispatch();
+  const handleAddNewBlank = () => {
+    dispatch(setQuestion({...question, choices: [...question.choices, ""], correctAnswer: [...question.correctAnswer, ""]}));
+  }
+
+  const handleRemoveBlank = (index: number) => {
+    dispatch(setQuestion({...question, choices: question.choices.filter((choice, i) => i !== index)}));
+  }
+
+  const handleUpdateCorrectAnswer = (index: number, val: string) => {
+    dispatch(setQuestion({...question, correctAnswer: question.correctAnswer.map((c, i) => i === index ? val : c)}));
+  }
+
   return(
     <div>
-
+      <ul>
+        {question.correctAnswer.map((answer, index) => (
+          <li key={index}>
+            <input type="text" value={answer} 
+            onChange={(e) => handleUpdateCorrectAnswer(index, e.target.value)}/>
+            <button onClick={()=> handleRemoveBlank(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <button className="btn btn-success" onClick={handleAddNewBlank}>Add New Blank</button>
     </div>)
 }
 export default MultipleBlanksEdit;
