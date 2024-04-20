16aea9fc9ef5df20b42f71019dd061256e91c6a9
samblock <block.sa@northeastern.edu>
2024-04-19 22:00:36 -0400

    saving

--- src/Kanbas/Courses/Quizzes/QuestionsEdit.tsx
+++ src/Kanbas/Courses/Quizzes/QuestionsEdit.tsx
@@ -1,1 +1,16 @@
-export {}
+import { useDispatch, useSelector } from "react-redux";
+import { KanbasState } from "../../store";
+
+const EditQuestions = () => {
+    const dispatch = useDispatch();
+    const questions = useSelector((state: KanbasState) => state.questionsReducer.questions);
+
+
+
+    return (
+        <div>
+            {}
+        </div>
+    );
+}
+export default EditQuestions;
