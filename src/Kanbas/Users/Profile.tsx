c8e1d15cc6b858b09d1659d353d062b968354a01
Dhivas-Sugumar <dhivassugumar@gmail.com>
2024-04-21 17:58:45 -0400

    Final changes

--- src/Kanbas/Users/Profile.tsx
+++ src/Kanbas/Users/Profile.tsx
@@ -7,6 +7,7 @@ export default function Profile() {
   const navigate = useNavigate();
   const fetchProfile = async () => {
     const account = await client.profile();
+    console.log(account);
     setProfile(account);
   };
   const save = async () => {
@@ -20,22 +21,42 @@ export default function Profile() {
     fetchProfile();
   }, []);
   return (
-    <div>
-      <h1>Profile</h1>
-      {profile && (
+<div>
+    <h1>Profile</h1>
+    {profile && (
+      <div>
         <div>
+          <label>Username:</label>
           <input value={profile.username} onChange={(e) =>
             setProfile({ ...profile, username: e.target.value })}/>
+        </div>
+        <div>
+          <label>Password:</label>
           <input value={profile.password} onChange={(e) =>
             setProfile({ ...profile, password: e.target.value })}/>
+        </div>
+        <div>
+          <label>First Name:</label>
           <input value={profile.firstName} onChange={(e) =>
             setProfile({ ...profile, firstName: e.target.value })}/>
+        </div>
+        <div>
+          <label>Last Name:</label>
           <input value={profile.lastName} onChange={(e) =>
             setProfile({ ...profile, lastName: e.target.value })}/>
+        </div>
+        <div>
+          <label>Date of Birth:</label>
           <input value={profile.dob} type="date" onChange={(e) =>
             setProfile({ ...profile, dob: e.target.value })}/>
+        </div>
+        <div>
+          <label>Email:</label>
           <input value={profile.email} onChange={(e) =>
             setProfile({ ...profile, email: e.target.value })}/>
+        </div>
+        <div>
+          <label>Role:</label>
           <select onChange={(e) =>
               setProfile({ ...profile, role: e.target.value })}>
             <option value="USER">User</option>
@@ -44,17 +65,13 @@ export default function Profile() {
             <option value="STUDENT">Student</option>
           </select>
         </div>
-      )}
-        <button onClick={save}>
-     Save
-  </button>
-  <button onClick={signout}>
-    Signout
-  </button>
-  <Link to="/Kanbas/Account/Admin/Users"
-    className="btn btn-warning w-100">
-    Users
-  </Link>
-    </div>
+      </div>
+    )}
+    <button onClick={save}>Save</button>
+    <button onClick={signout}>Signout</button>
+    <Link to="/Kanbas/Account/Admin/Users" className="btn btn-warning w-100">
+      Users
+    </Link>
+  </div>
   );
 }
